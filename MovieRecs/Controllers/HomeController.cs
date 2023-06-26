2c838922995284bd04c0eb6076f7179c297eef67
BW-BW <brianwidjaja53@gmail.com>
2023-06-26 11:36:42 +0800

    Success call api, need to map

--- MovieRecs/Controllers/HomeController.cs
+++ MovieRecs/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieRecs.Models;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MovieRecs.Controllers
 {
@@ -8,9 +10,41 @@ namespace MovieRecs.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private readonly HttpClient _httpClient;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> CallApi()
+        {
+            string apiUrl = "https://moviesminidatabase.p.rapidapi.com/movie/byGen/Comedy/"; // Replace with the actual API endpoint URL
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, apiUrl);
+            request.Headers.Add("X-RapidAPI-Key", "87429eac32msh305b85d98fca914p1122fajsnad4deb35ba9c"); // Replace with your RapidAPI key
+            request.Headers.Add("X-RapidAPI-Host", "moviesminidatabase.p.rapidapi.com");
+
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine("nice");
+                // Process the response data as needed
+                return Ok(responseBody); // Return the response data as the HTTP response
+                //return View(responseBody);
+                //return View();
+
+            }
+            else
+            {
+                // Handle the error response
+                System.Diagnostics.Debug.WriteLine("boo");
+                return StatusCode((int)response.StatusCode); // Return the error status code as the HTTP response
+                
+            }
         }
 
         public IActionResult Index()
---
+++ MovieRecs/Models/ResponseAPI.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieRecs.Models
+{
+    public class ResponseAPI
+    {
+        public List<MovieResultModel> Results { get; set; }
+    }
+
+    public class MovieResultModel
+    {
+        public string imdb_id { get; set; }
+        public string title { get; set; }
+    }
+}
